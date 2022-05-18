15afa2c3a4cd8d477b0873af61902f9d018323d7
Topskys <85061657+Topskys@users.noreply.github.com>
2022-05-19 00:24:51 +0800

    Create debounce.js

---
+++ debounce.js
@@ -0,0 +1,27 @@
+//防抖
+			//延迟打印节点元素的value
+			function Debounce(callback, delay) {
+				let timer = null;
+				return function run(e) {
+					if (timer !== null) {
+						clearTimeout(timer)
+					}
+					timer = setTimeout(() => {
+						callback(e)
+					}, delay)
+				}
+			}
+
+			function printValue(e) {
+				console.log(e.target.value)
+			}
+      
+      //input標籤
+			let debounce = document.querySelector("#debounce")
+
+			//html加载完毕后运行！
+			window.onload = () => {
+				console.log("html加载完毕！");
+				//根据id绑定输入事件
+				debounce.oninput = Debounce(printValue, 500)
+			}
